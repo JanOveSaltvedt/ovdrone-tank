1acdf5a071a946bf3c7c49457f36a3868e963f24
Jan Ove Saltvedt <jan.ove@saltvedt.no>
2014-12-28 13:44:28 +0100

    Wrote receiving framework for network communication

--- ComClient.cpp
+++ ComClient.cpp
@@ -2,7 +2,7 @@
 #include <sstream>
 #include <chrono>
 #include <iostream>
-#include "ovdrone.pb.h"
+
 
 using namespace std;
 using namespace ovdrone;
@@ -28,9 +28,8 @@ ComClient::ComClient(string target_host)
         m_socket.set_option(option);
 
 		// Start the io service in a new thread
+        startReadHeader();
 		m_ioThread = thread(&ComClient::runIoService, this);
-
-        SendPing();
  	}
 	catch( boost::system::system_error &err ) {
 		cout << "[ComClient] Error in connect: " << err.what() << endl;
@@ -44,8 +43,10 @@ ComClient::~ComClient() {
 void ComClient::runIoService() {
 	cout << "Starting ComClient io service" << endl;
 	m_ioService.run();
+    cout << "The io service for ComClient ran out of work to do." << endl;
 }
 
+/*
 void ComClient::SendPing() {
 
 	auto now = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch());
@@ -53,25 +54,109 @@ void ComClient::SendPing() {
     msg.set_request_timestamp(now.count());
     msg.set_response_timestamp(now.count());
 
-    size_t payloadSize = msg.ByteSize();
+    SendMessage(&msg, proto::PING);
+}
+*/
 
-	m_writeBuffer.resize(header_size + payloadSize);
+void ComClient::SendMessage(google::protobuf::Message *msg, proto::MessageTypes msgType) {
+    // Ensure that only one thread is sending at the same time
+    std::lock_guard<std::mutex> lock(m_sendMutex);
 
-	// Magic number that is my birthday
-	*((uint32_t*)&m_writeBuffer[0]) = htobe32(14041992);
-	*((uint32_t*)&m_writeBuffer[4]) = htobe32(payloadSize); // Payload size
-    m_writeBuffer[8] = ovdrone::proto::PING; // Message type
+    size_t payloadSize = msg->ByteSize();
+
+    m_writeBuffer.resize(HEADER_SIZE + payloadSize);
+
+    // Magic number that is my birthday
+    *((uint32_t*)&m_writeBuffer[0]) = htobe32(14041992);
+    *((uint32_t*)&m_writeBuffer[4]) = htobe32(payloadSize); // Payload size
+    m_writeBuffer[8] = (uint8_t)msgType; // Message type
 
 
-	if(!msg.SerializeToArray(&m_writeBuffer[header_size], payloadSize)) {
-		cout << "[ComClient] Could not serialize ping to buffer" << endl;
-	}
+    if(!msg->SerializeToArray(&m_writeBuffer[HEADER_SIZE], payloadSize)) {
+        cout << "[ComClient] Could not serialize message to buffer" << endl;
+    }
 
-	try {
-		// Write to the socket
-		boost::asio::write(m_socket, boost::asio::buffer(m_writeBuffer));
-	}
-	catch( boost::system::system_error &err ) {
-		cout << "[ComClient] Error in SendPing: " << err.what() << endl;
-	}
+    try {
+        // Write to the socket
+        boost::asio::write(m_socket, boost::asio::buffer(m_writeBuffer));
+    }
+    catch( boost::system::system_error &err ) {
+        cout << "[ComClient] Error in SendMessage: " << err.what() << endl;
+    }
+}
+
+
+void ComClient::startReadHeader() {
+    m_readBuffer.resize(HEADER_SIZE); // Resize to the header size
+    boost::asio::async_read(m_socket, boost::asio::buffer(m_readBuffer), std::bind(&ComClient::handleReadHeader, shared_from_this(), std::placeholders::_1));
+}
+
+void ComClient::handleReadHeader(const boost::system::error_code &ec) {
+    if(!ec) {
+        uint32_t magicNumber = be32toh(*(uint32_t*)(&m_readBuffer[0]));
+        if(magicNumber != 14041992) {
+            cout << "Received invalid magic number" << endl;
+            startReadHeader();
+            return;
+        }
+        size_t payloadSize = getPayloadSize();
+        //uint8_t msgType = m_readBuffer[8]
+        //qDebug() << "Got header";
+        startReadBody(payloadSize);
+    }
+    else {
+        cout << "Error in handleReadHeader!" << endl;
+        //startReadHeader();
+    }
+}
+
+size_t ComClient::getPayloadSize() {
+    return be32toh(*(uint32_t*)(&m_readBuffer[4]));
+}
+
+void ComClient::startReadBody(size_t payloadSize) {
+    m_readBuffer.resize(HEADER_SIZE+payloadSize); // Resize to the header size + payload
+    boost::asio::async_read(m_socket, boost::asio::buffer(&m_readBuffer[HEADER_SIZE], payloadSize), std::bind(&ComClient::handleReadBody, shared_from_this(), std::placeholders::_1));
+}
+
+void ComClient::handleReadBody(const boost::system::error_code &ec) {
+    if(!ec) {
+        cout << "Great we received a frame!" << endl;
+
+        uint8_t msgType = m_readBuffer[8];
+
+        switch(msgType) {
+        case ovdrone::proto::PING:
+            handlePing();
+            break;
+        default:
+            cout << "Unknown message with type " << msgType << " recieved" << endl;
+        }
+
+        startReadHeader();
+    }
+    else {
+        cout << "Error in handleReadBody!" << endl;
+        startReadHeader();
+    }
+}
+
+void ComClient::handlePing() {
+    auto now = chrono::duration_cast<chrono::milliseconds>(chrono::system_clock::now().time_since_epoch());
+    ovdrone::proto::Ping msg;
+
+    if(!msg.ParseFromArray(&m_readBuffer[HEADER_SIZE], getPayloadSize())) {
+        cout << "Could not decode the ping message" << endl;
+        return;
+    }
+
+    if(!msg.IsInitialized()) {
+        cout << "Received an incomplete ping message" << endl;
+        return;
+    }
+
+
+    uint64_t timestamp = now.count();
+    msg.set_response_timestamp(timestamp);
+    SendMessage(&msg, ovdrone::proto::PING);
 }
--- ComClient.h
+++ ComClient.h
@@ -5,28 +5,42 @@
 #include <string>
 #include <opencv2/opencv.hpp>
 #include <boost/asio.hpp>
+#include "ovdrone.pb.h"
+#include <mutex>
 
 
 namespace ovdrone {
 
-class ComClient {
+class ComClient : public std::enable_shared_from_this<ComClient> {
 public:
 	ComClient(std::string target_host);
 	~ComClient();
 
-	void SendPing();
+    void SendMessage(google::protobuf::Message* msg, ovdrone::proto::MessageTypes msgType);
+
 
 private:
 	void runIoService();
 
+    void startReadHeader();
+    void handleReadHeader(const boost::system::error_code &ec);
+    size_t getPayloadSize();
+
+    void startReadBody(size_t payloadSize);
+    void handleReadBody(const boost::system::error_code &ec);
+
+    void handlePing();
+
 private:
+
 	std::string m_remoteHost;
 	std::thread m_ioThread;
 	boost::asio::io_service m_ioService;
 	boost::asio::ip::tcp::socket m_socket;
 	boost::asio::ip::tcp::endpoint m_endpoint;
+    std::mutex m_sendMutex;
 
-	enum { header_size = 9 };
+    enum { HEADER_SIZE = 9 };
 	std::vector<uint8_t> m_readBuffer;
 	std::vector<uint8_t> m_writeBuffer;
 };
